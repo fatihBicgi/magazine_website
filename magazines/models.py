7ac7e3345bb14c24a48501e0db76613d40d13089
Fatih Bicgi <fatihbcg@gmail.com>
2022-02-09 01:29:07 +0300

    sorting system changed

--- magazines/models.py
+++ magazines/models.py
@@ -11,8 +11,7 @@ class Magazine(models.Model):
     created_date =models.DateTimeField(auto_now_add=True)
     isPublished=models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ['-created_date',]
+    
 
     def __str__(self):
         return self.name
--- magazines/views.py
+++ magazines/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView
 # Create your views here.
 # http://127.0.0.1:8000/
 def index(request):
-    magazines= Magazine.objects.all()
+    magazines= Magazine.objects.all().order_by('-created_date')
     context = {
         'magazines' : magazines
     }
